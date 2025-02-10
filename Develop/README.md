cb18828fbf81ff7269d76c5fe77b50c9ed62c723
cholmun <c.holmun55@gmail.com>
2025-02-09 22:25:27 -0500

    add index.js, gitignore file, and example README file

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+
+# example
+
+## Description
+an example of my readme generator
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+npm i
+
+## Usage
+follow prompts in command line to generate readme
+
+## License
+This project is licensed under the None license.
+
+## Contributing
+skip
+
+## Tests
+skip
+
+## Questions
+If you have any questions, please feel free to contact me:
+- GitHub: [cholmun](https://github.com/cholmun)
+- Email: c.holmun55@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,97 @@
-// TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Define the questions to prompt the user
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to generate the README content
+function generateREADME(answers) {
+  return `
+# ${answers.title}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${answers.description}
 
-// Function call to initialize app
-init();
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions, please feel free to contact me:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+`;
+}
+
+// Prompt the user for input and generate the README file
+inquirer.prompt(questions).then((answers) => {
+  const readmeContent = generateREADME(answers);
+  fs.writeFileSync('README.md', readmeContent);
+  console.log('README.md file has been generated successfully!');
+});
